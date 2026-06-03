8fdc3cac61ca840da602c48d12098f4a9cd99430
xjannebaznivy-design <xjan.nebaznivy@student.campanus.cz>
2026-06-03 08:12:05 +0200

    Fix: Rename 'answar' to 'answer' in data.py

--- data.py
+++ data.py
@@ -1,37 +1,37 @@
 data = [
   {"text":"Fungují počítače na 0 a 1 neboli binární soustavě?",
-   "answar": True,
+   "answer": True,
    "choses": "Napiš True nebo False"},
 
   {"text": "Jaký je rozdíl mezi malwarem a virem",
-   "answar": "c",
+   "answer": "c",
    "choses": "a: malware je nebezpečnější než vir, b: vir je nebezpečnější neř malware, c: malware je slovo nadřazené viru"},
 
   {"text": "Application Programming Interface(API) je programovací jazyk",
-   "answar": False,
+   "answer": False,
    "choses": "True nebo False"},
 
   {"text": "Python rozeznává 9 ZÁKLADNÍCH datových typů: str, int, float, dict, list, set, tuple, set a None",
-   "answar": True,
+   "answer": True,
    "choses": "True nebo False"},
 
   {"text": "Jsou HTML a CSS programovací jazyky?",
-   "answar": False,
+   "answer": False,
    "choses": "True nebo False"},
 
   {"text": "Artifical Intelligence(AI) funguje díky:",
-   "answar": "b",
+   "answer": "b",
    "choses": "a: Umí myslet, b: Přijímá hodně dat, které analyzuje a hledá v nich souvislosti, c: Za Ai se nachází skutečný člověk"},
 
   {"text": "Co je nejdůležitější software v PC?",
-   "answar": "a",
+   "answer": "a",
    "choses": "a: operační systém, b: pruzkumník souborů, c: terminál"},
 
   {"text": "je class obecný předpis?",
-   "answar": True,
+   "answer": True,
    "choses": "True nebo False"},
 
   {"text": "Programovací jazyky jsou prostředek pro komunikaci s počítačem",
-   "answar": True,
+   "answer": True,
    "choses": "True nebo False"},
 ]
